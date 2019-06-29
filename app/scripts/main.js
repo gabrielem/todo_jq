fa8b40d134b79d923cad496589ded35b59a7f7c0
gabrielem_ <gabriele.marazzi@gmail.com>
2019-06-29 14:50:17 +0200

    Add logic for add item

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,3 +7,23 @@ console.log('\'Allo \'Allo!');
 // Uncomment to enable Bootstrap popovers
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
+$( document ).ready(function() {
+    $('#textItem').focus()
+    $("#textItem").keydown(function (e) {
+      if (e.keyCode == 13) {
+        addItem();
+      }
+    });
+});
+
+let todos = []
+
+const addItem = function(e) {
+  if (e) {e.preventDefault()}
+  console.log('textItem', $('#textItem').val());
+  if (!$('#textItem').val()) {
+    alert('You can not insert and empty item!')
+  } else {
+      todo.push($('#textItem').val())
+  }
+}
